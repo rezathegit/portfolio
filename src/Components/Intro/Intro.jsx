f50406a3131972d1780c39d07bff4cd9ba288ba9
Salt Dev <asadimohamadreza@gmail.com>
2022-01-28 08:17:32 +0100

    menu added

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -2,6 +2,11 @@ import "./Intro.scss";
 
 export default function Intro() {
   return <div className="intro" id="intro">
-Intro
+<div className="left">
+  <div className="imageContainer">
+    <img src="assests/Reza-Asadi.jpeg" alt="" />
+  </div>
+</div>
+<div className="right"></div>
   </div>;
 }
