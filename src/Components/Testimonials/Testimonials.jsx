df7eb7cb2ab08ef1f407d5585a09dbd74ee432ce
Salt Dev <asadimohamadreza@gmail.com>
2022-02-02 13:33:49 +0100

    testimonials section added

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,27 +1,65 @@
 import "./Testimonials.scss";
 
 export default function Testimonials() {
+
+  const data = [
+    {
+      id: 1,
+      name: "Milos Tanaskovic",
+      title: "Senior Developer",
+      img:
+        "https://avatars.githubusercontent.com/u/45362409?v=4",
+      icon: "assets/twitter.png",
+      desc:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem.",
+    },
+    {
+      id: 2,
+      name: "Jonatan Petterson",
+      title: "Co-Founder of Metanet",
+      img:
+        "https://avatars.githubusercontent.com/u/77250488?v=4",
+      icon: "assets/youtube.png",
+      desc:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ",
+      featured: true,
+    },
+    {
+      id: 3,
+      name: "Andrew Chalkley",
+      title: "Treehouse Guest Teacher",
+      img:
+        "https://uploads.teamtreehouse.com/production/profile-photos/26170/avatar_TeacherShoot-Andrew.jpg",
+      icon: "assets/linkedin.png",
+      desc:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
+    },
+  ];
+
   return <div className="testimonials" id="testimonials">
 <h1>Testimonials</h1>
 <div className="container">
-  <div className="card">
+  {data.map(d=>(
+
+    <div className={d.featured ? "card featured" : "card"}>
     <div className="top">
       <img className="left"
-      src="assets/right-arrow.png" alt="" />
+      src={d.icon} alt="" />
       <img 
       className="user"
-      src="https://media-exp1.licdn.com/dms/image/C4E03AQFP0Y86H16mWw/profile-displayphoto-shrink_400_400/0/1640534093386?e=1649289600&v=beta&t=MHD1Uo5wf4Vwo-_Q4lJEdkA6cuAeIeNkxKnH5OBuVsI" alt="" />
+      src={d.img} 
+      alt="" />
       <img className="right" src="assets/youtube.png" alt="" />
     </div>
     <div className="center">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-      Sit natus necessitatibus.
+      {d.desc}
     </div>
     <div className="bottom">
-      <h3>Milos</h3>
-      <h4>Fullstack we developer at Footway</h4>
+      <h3>{d.name}</h3>
+      <h4>{d.title}</h4>
     </div>
   </div>
+      ))}
 </div>
   </div>;
 }
