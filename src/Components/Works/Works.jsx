cd9ad3e40bf4fd494e63623e271af8a1b7238adf
Salt Dev <asadimohamadreza@gmail.com>
2022-02-02 10:55:39 +0100

    works section created

--- src/Components/Works/Works.jsx
+++ src/Components/Works/Works.jsx
@@ -2,6 +2,25 @@ import "./Works.scss";
 
 export default function Works() {
   return <div className="works" id="works">
-      Works
+    <div className="slider">
+      <div className="container">
+        <div className="item">
+          <div className="left">
+            <div className="leftContainer">
+              <div className="imgContainer">
+                <img src="assests/mobile.png" alt="" />
+              </div>
+              <h2>Title</h2>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                Similique et repellat magni velit a repudiandae deleniti voluptate!
+              </p>
+              <span>Projects</span>
+            </div>
+          </div>
+          <div className="right"></div>
+        </div>
+      </div>
+    </div>
   </div>;
 }
