ed7ac83d5f50226284a47e86942c28cc9eeacdb7
Salt Dev <asadimohamadreza@gmail.com>
2022-02-03 14:04:38 +0100

    contact section added

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,18 +1,29 @@
+import { useState } from "react"
 import "./Contact.scss"
 
 export default function Contact() {
+
+    const [message, setMessage] = useState(false)
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setMessage(true);
+    }
+
     return <div className="contact" id="contact">
         <div className="left">
             <img src="assets/shake.svg" alt="" />
         </div>
         <div className="right">
             <h2>Contact.</h2>
-            <form>
-                <input type="text" placeholder="Email"/>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Email" />
                 <textarea placeholder="Message"></textarea>
                 <button type="submit" >Send</button>
+                {message && <span>Thanks! I'll come back to you ASAP!</span> }
+          
             </form>
+            
         </div>
-Contact
+        Contact
     </div>;
 }
