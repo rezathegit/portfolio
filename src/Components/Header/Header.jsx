b19908e6c58388e5ae51de0acbecc2d66a44b6db
Salt Dev <asadimohamadreza@gmail.com>
2022-02-08 11:56:03 +0100

    header link fixed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import "./Header.scss";
-import {Person, Mail, LinkedIn} from "@mui/icons-material";
+import { Person, Mail, LinkedIn } from "@mui/icons-material";
 
 
 export default function Header({ menuOpen, setMenuOpen }) {
@@ -7,17 +7,19 @@ export default function Header({ menuOpen, setMenuOpen }) {
     <div className="wrapper">
       <div className="left">
         <a href="#intro" className="logo">REZA.</a>
-     <div className="itemContainer">
-    <LinkedIn className="icon"/>
-    <span>LinkedIn</span>
-     </div>
-     <div className="itemContainer">
-    <Mail className="icon"/>
-    <span>reza.asadi@appliedtechnology.se</span>
-     </div>
+        <div className="itemContainer">
+          <a href="https://www.linkedin.com/in/reza-asadi-/" target="_blank"  rel="noopener noreferrer">
+            <LinkedIn className="icon" />
+            <span>LinkedIn</span>
+          </a>
+        </div>
+        <div className="itemContainer">
+          <Mail className="icon" />
+          <span>reza.asadi@appliedtechnology.se</span>
+        </div>
       </div>
       <div className="right">
-        <div className="hamburger" onClick={() =>setMenuOpen(!menuOpen)} >
+        <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)} >
           <span className="line1"></span>
           <span className="line2"></span>
           <span className="line3"></span>
